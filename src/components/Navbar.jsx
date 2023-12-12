139da8839847115a178dae9c314b33824ce5fef0
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-12-12 23:03:10 +0600

    navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,52 +1,80 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
+
+const NavLinks = ({ isMobile = false, closeMobileMenu }) => {
+  const location = useLocation();
+
+  const links = [
+    { to: "/", text: "Home" },
+    { to: "/todo/create", text: "Create Todo" },
+    { to: "/user/register", text: "Register" },
+    { to: "/user/login", text: "Login" },
+    { to: "/user/profile", text: "Profile" },
+  ];
+
+  const linkClickHandler = () => {
+    if (isMobile) {
+      closeMobileMenu();
+    }
+  };
+
+  return (
+    <div className={isMobile ? "block" : "hidden md:flex space-x-4"}>
+      {isMobile && (
+        <div className="bg-white p-4 rounded shadow-lg my-1 absolute right-4 px-10">
+          {links.map((link) => (
+            <NavLink
+              key={link.to}
+              to={link.to}
+              className={`block text-gray-800 hover:text-gray-600 my-2 text-center ${
+                location.pathname === link.to ? "font-bold text-lg" : ""
+              }`}
+              onClick={linkClickHandler}
+            >
+              {link.text}
+            </NavLink>
+          ))}
+        </div>
+      )}
+
+      {!isMobile &&
+        links.map((link) => (
+          <NavLink
+            key={link.to}
+            to={link.to}
+            className={`text-white hover:text-green-500 ${
+              location.pathname === link.to ? "border-b-2 border-white" : ""
+            }`}
+            onClick={linkClickHandler}
+          >
+            {link.text}
+          </NavLink>
+        ))}
+    </div>
+  );
+};
 
 const Navbar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
   return (
     <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        {/* Logo or Site Title */}
+      <div className="max-w-screen-xl mx-auto flex justify-between items-center">
+        {/* Logo */}
         <Link to="/" className="text-white text-xl font-bold">
           TODO
         </Link>
 
         {/* Navigation Links */}
-        <div className="hidden md:flex space-x-4">
-          <NavLink
-            to="/"
-            className="text-white hover:text-gray-300"
-            activeClassName="border-b-2 border-white"
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/user/register"
-            className="text-white hover:text-gray-300"
-            activeClassName="border-b-2 border-white"
-          >
-            Register
-          </NavLink>
-          <NavLink
-            to="/user/login"
-            className="text-white hover:text-gray-300"
-            activeClassName="border-b-2 border-white"
-          >
-            Login
-          </NavLink>
-          <NavLink
-            to="/user/profile"
-            className="text-white hover:text-gray-300"
-            activeClassName="border-b-2 border-white"
-          >
-            Profile
-          </NavLink>
-        </div>
+        <NavLinks isMobile={false} />
 
         {/* Mobile Navigation (Hidden by default) */}
         <div className="md:hidden">
-          {/* Add a responsive menu icon (e.g., hamburger menu) */}
-          {/* This can be a component or an SVG icon */}
-          <button>
+          <button onClick={toggleMobileMenu}>
             <svg
               className="text-white h-6 w-6"
               fill="none"
@@ -62,6 +90,9 @@ const Navbar = () => {
               ></path>
             </svg>
           </button>
+          {mobileMenuOpen && (
+            <NavLinks isMobile={true} closeMobileMenu={toggleMobileMenu} />
+          )}
         </div>
       </div>
     </nav>
