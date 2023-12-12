1625e07af1f40c88fdccb4e72bfbf037cf9ad799
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-12-13 01:23:15 +0600

    login form created using react form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
-function Login() {
-  return <div>Login</div>;
-}
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded shadow-md">
+      <h2 className="text-2xl font-semibold mb-4">Login</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4">
+          <label className="block text-gray-600">Email:</label>
+          <input
+            type="email"
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Invalid email address",
+              },
+            })}
+          />
+          {errors.email && (
+            <span className="text-red-500">{errors.email.message}</span>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-600">Password:</label>
+          <input
+            type="password"
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters",
+              },
+            })}
+          />
+          {errors.password && (
+            <span className="text-red-500">{errors.password.message}</span>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
