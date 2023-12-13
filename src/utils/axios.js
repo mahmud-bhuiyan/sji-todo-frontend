c8662cb4550fb3230cfc34cc3dfd6b16f581cf07
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-12-13 14:55:27 +0600

    axios added from sji project

---
+++ src/utils/axios.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+// const apiURL = "https://jsonplaceholder.typicode.com";
+const apiURL = "https://jsonplaceholder.typicode.com";
+
+export const axiosOpen = axios.create({
+  baseURL: apiURL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+export const axiosSecure = axios.create({
+  baseURL: apiURL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+  },
+});
+
+// using interceptors
+
+export const axiosNonSecureInstance = axios.create({
+  baseURL: process.env.API_BASE_URL,
+});
+
+export const axiosSecureInstance = axios.create({
+  baseURL: process.env.API_BASE_URL,
+});
+
+axiosSecureInstance.interceptors.request.use(
+  (config) => {
+    const userToken = localStorage.userToken
+      ? localStorage.getItem("userToken")
+      : null;
+    config.headers.Authorization = `Bearer ${userToken}`;
+    return config;
+  },
+  (error) => {
+    // Handle request error
+    return Promise.reject(error);
+  }
+);
